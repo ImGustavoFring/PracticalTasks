51b6ef0084a13fffcbe879b4425f7b141a0d4801
Gustavo Fring <gustavofring254@gmail.com>
2024-08-02 15:15:50 +0500

    first-task-v-1

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Run();
-            
+            // мне объяснили сделать какое-то изменение, чтобы реализовать merge request, которого, как опции не существует
+            Run();  
         }
         
         private static void Run ()
