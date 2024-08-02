80979ec65c2d0cd218c0ce7b046a18ce28127b32
Gustavo Fring <gustavofring254@gmail.com>
2024-08-02 14:51:36 +0500

    Add project files.

---
+++ FirstTask/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            
+        }
+        
+        private static void Run ()
+        {
+            Console.WriteLine(StringChanger.Change("a"));
+            Console.WriteLine(StringChanger.Change("abcdef"));
+            Console.WriteLine(StringChanger.Change("abcde"));
+        }
+    }
+}
---
+++ FirstTask/StringChanger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class StringChanger
+    {
+        public static string Change(string text)
+        {
+            bool hasEvenLength = text.Length % 2 == 0;
+
+            if (hasEvenLength)
+            {
+                return ChangeWithEvenLength(text);
+            }
+            else
+            {
+                return ChangeWithOddLength(text);
+            }
+        }
+        private static string ChangeWithEvenLength(string text)
+        {
+            var result = new StringBuilder();
+            int startOfSecondHalf = text.Length / 2;
+
+            string firstHalf = text.Substring(0, startOfSecondHalf);
+            string secondHalf = text.Substring(startOfSecondHalf);
+
+            result.Append(new string(firstHalf.Reverse().ToArray()));
+            result.Append(new string(secondHalf.Reverse().ToArray()));
+
+            return result.ToString();
+        }
+
+        private static string ChangeWithOddLength(string text)
+        {
+            var result = new StringBuilder();
+
+            string reversedText = new string(text.Reverse().ToArray());
+            result.Append(reversedText);
+            result.Append(text);
+
+            return result.ToString();
+        }
+    }
+}
