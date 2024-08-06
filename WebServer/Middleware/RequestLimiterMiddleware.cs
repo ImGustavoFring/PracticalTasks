54918c3360cda18a12d5e86f865c566208f62677
Gustavo Fring <gustavofring254@gmail.com>
2024-08-06 12:30:17 +0500

    ninth-task-v-1

---
+++ WebServer/Middleware/RequestLimiterMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace WebServer.Middleware
+{
+    public class RequestLimiterMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private static int _currentRequests = 0;
+        private readonly int _parallelLimit;
+
+        public RequestLimiterMiddleware(RequestDelegate next, int parallelLimit)
+        {
+            _next = next;
+            _parallelLimit = parallelLimit;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (Interlocked.Increment(ref _currentRequests) > _parallelLimit)
+            {
+                Interlocked.Decrement(ref _currentRequests);
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Сервис недоступен: слишком много запросов.");
+                return;
+            }
+
+            try
+            {
+                await _next(context);
+            }
+            finally
+            {
+                Interlocked.Decrement(ref _currentRequests);
+            }
+        }
+    }
+}
